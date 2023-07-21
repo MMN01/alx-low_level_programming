2962745a61aaf129b69e6a4c40af147bd015cd5c
MMN01 <mnyahya007@gmail.com>
2023-07-21 18:08:58 -0400

    new update

--- 0x04-more_functions_nested_loops/0-main.c
+++ 0x04-more_functions_nested_loops/0-main.c
@@ -2,10 +2,10 @@
 #include <stdio.h>
 int main(void)
 {
-    char c;
-    c = 'A';
-    printf("%c: %d\n", c, _isupper(c));
-    c = 'a';
-    printf("%c: %d\n", c, _isupper(c));
-    return (0);
+char c;
+c = 'A';
+printf("%c: %d\n", c, _isupper(c));
+c = 'a';
+printf("%c: %d\n", c, _isupper(c));
+return (0);
 }
