8ea5c417a66f3bc66e95ddfa0e25b0cd85d103b7
MMN01 <mnyahya007@gmail.com>
2023-07-21 01:52:27 +0100

    'alo'

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,5 +1,9 @@
 #include "main.h"
-/** * _isupper - checks for uppercase character * @c: Variable text * Return: Always 0. */
+/** 
+* _isupper - checks for uppercase character
+* @c: Variable text
+* Return: Always 0. 
+*/
 int _isupper(int c)
 {
  if (c >= 65 && c <= 90)
